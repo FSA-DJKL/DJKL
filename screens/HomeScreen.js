eb124321cfab3176314e686c9876c87a2a60a5b1
Lindsey Zurovchak <zurovchaklindsey@gmail.com>
2020-05-31 00:52:41 -0400

    initial commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,56 +1,126 @@
 import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
+import React, { Component } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios'
 import { MonoText } from '../components/StyledText';
+const APIKey = require('../clientId.json').googleAPI
+export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      label:''
+    }
+    this.takePhoto = this.takePhoto.bind(this);
+    this.handleImage = this.handleImage.bind(this);
+  }
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+  // static navigationOptions = {
+  //   headerTitle: <LogoTitle />,
+  // };
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+  async componentDidMount() {
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    await Permissions.askAsync(Permissions.CAMERA);
+  }
+  async takePhoto() {
+    // let imageData = await ImagePicker.launchCameraAsync({
+    //   //   allowsEditing: true,
+    // //   aspect: [4, 3],
+    //   base64: true,
+    // });
+    let imageData = await ImagePicker.launchImageLibraryAsync({
+      base64:true
+    })
 
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
+    this.handleImage(imageData);
+  }
 
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
+  async handleImage(imageData) {
+    try {
+      if (!imageData.cancelled) {
+        //do things
+        const data = await axios.post(
+          `https://us-vision.googleapis.com/v1/images:annotate?key=${APIKey}`,
+          imageData,
+          {
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            }}
+          )
+          console.log(data)
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+  render() {
+    return (
+      <View >
+        <View>
+          <TouchableOpacity onPress={this.takePhoto} >
+            <View>
+              <Ionicons name="ios-camera" size={75} color={'white'} />
+            </View>
           </TouchableOpacity>
         </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
+
+// export default function HomeScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+//         <View style={styles.welcomeContainer}>
+//           <Image
+//             source={
+//               __DEV__
+//                 ? require('../assets/images/robot-dev.png')
+//                 : require('../assets/images/robot-prod.png')
+//             }
+//             style={styles.welcomeImage}
+//           />
+//         </View>
+
+//         <View style={styles.getStartedContainer}>
+//           <DevelopmentModeNotice />
+
+//           <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
+//          <Button title='Test'/>
+
+//           <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+//             <MonoText>screens/HomeScreen.js</MonoText>
+//           </View>
+
+//           <Text style={styles.getStartedText}>
+//             Change any of the text, save the file, and your app will automatically reload.
+//           </Text>
+//         </View>
+
+//         <View style={styles.helpContainer}>
+//           <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+//             <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </ScrollView>
+
+//       <View style={styles.tabBarInfoContainer}>
+//         <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+
+//         <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+//           <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
 
 HomeScreen.navigationOptions = {
   header: null,
